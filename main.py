e1f709111e0bbf037d9de75a6236dbf8560042ac
22344Hunter <134021741+22344Hunter@users.noreply.github.com>
2023-06-08 12:45:54 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-get_number = int(input("Choose a number?"))
-times_five = get_number * 5
-print("{} times five is equal to "
-"{}".format(get_number, times_five)) 
+show_instructions = input("Have you played this game before?").lower()
+
+if show_instructions == "yes":
+  print("program continues")
+
+elif show_instructions == "y":
+  print("program continues")
+  
+elif show_instructions == "no":
+  print("display instructions")
+
+elif show_instructions == "n":
+  print("display instructions")
+  
+else:
+  print("Please answer yes/no")
